eeeb78a2a68663c82304d9ebdb8321b258293a98
Jesus Vazquez <jesusvb314@gmail.com>
2026-05-28 15:47:05 -0600

    correccion del archivo py

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 echo "activando el entorno virtual"
-
-if[! "-d venv"];then
+# Verificar si el entorno virtual existe 
+if [! "-d venv"];then
     python3 -m venv venv 
 fi
 
-source venv/bin/activate
+# Activar el entorno virtual correctamente 
+
+if [-f "venv/bin/activate"];then
+   source venv/bin/activate
+elif [-f "venv/Scripts/activate"];then # Para Windows
+    source venv/Scripts/activate
+else 
+    echo "Error: No se pudo activar el entorno virtual"
+    exit 1
+fi
+
+#Verificar si pip esta instalado correctamente
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"
